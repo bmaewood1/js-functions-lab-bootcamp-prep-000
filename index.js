e1a04991f14bc81ecfc385eeab5d2527403fef29
Bailey Wood <woodbmae@gmail.com>
2019-07-23 18:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // write your code below!
+
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+function happyHolidayTo(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+  return `${holiday}, ${name}!`
+}
+
+function HolidayCountdown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+  return `${days}, ${holiday}!`
+}
+
+happyHolidays()
+
+happyHolidaysTo('you')
+
+happyHolidayTo("Independence Day", "you")
+
+HolidayCountdown("20", "Mother's Day")
+
