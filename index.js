b7d2959e17ceae1e18e375eea99264f4396b3c84
Bailey Wood <woodbmae@gmail.com>
2019-07-23 18:41:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
 
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
@@ -10,20 +13,8 @@ function happyHolidayTo(holiday, name){
 
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`;
+}
 
 
 
-
-
-
-
-
-
-happyHolidays()
-
-happyHolidaysTo('you')
-
-happyHolidayTo("Independence Day", "you")
-
-HolidayCountdown("20", "Mother's Day")
 
